c1882c0676614c96ceffa8f1ed325e719a8a883e
albaye <jy317@ic.ac.uk>
2020-05-05 12:35:34 +0200

    Part 2 - Buffering and Streaming Data

--- client.py
+++ client.py
@@ -1,28 +1,37 @@
 import socket
 
+HEADERSIZE = 10
 # create the socket
 # AF_INET ==ipv4
 # SOCK_STREAM == TCP (connection oriented)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server.
-s.connect((socket.gethostname(), 1234))
+s.connect((socket.gethostname(), 1235))
 
 # Use a while loop to be able to receive more than 1 message.
 while True:
     full_msg = ''
+    new_msg = True
 
-    # Use a while loop to retrieve the whole msg.
-    # No matter what buffer size we receive at a time
-    # I may not be long enough.
     while True:
-        # Attempt to receive data in buffer of 8 bytes at a time.
-        msg = s.recv(8)
-        # If message is finished, exit the while loop.
-        if len(msg) <= 0:
-            break
+        # Use 16 bytes to receive a bit more than the header
+        msg = s.recv(16)
+        
+        # If there is a new message, get the header to know the length of the message
+        if new_msg:
+            print(f"new message length : {msg[:HEADERSIZE]}")
+            # With int, python ignores the spaces appended.
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
         full_msg += msg.decode("utf-8")
 
-    # Print the message if there is one.
-    if len(full_msg) > 0:
-        print(full_msg)
+        # if the full message is received, print it and emtpy the variable full_msg
+        # also need to set new_msg to true, to be able to get the header of the ew message.
+        if len(full_msg) - HEADERSIZE == msglen:
+            print("full message received")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+            
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
+import time
 
+HEADERSIZE = 10
 # create the socket
 # AF_INET ==ipv4
 # SOCK_STREAM == TCP (connection oriented)
@@ -7,7 +9,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # For IP sockets, the address we bind to is a tuple of the hostname and the port number
 # In this case port number is 1234
-s.bind((socket.gethostname(), 1234))
+s.bind((socket.gethostname(), 1235))
 
 # Make a queue of 5
 # If full, it will reject connection
@@ -17,7 +19,19 @@ while True:
     # now our endpoint knows about the OTER end point.
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established.")
+
+    msg = "Welcome to the server!"
+    # The first part of the msg is a header.
+    # the :<20 will append spaces at the end of the length (left align())
+    # in this case msg has length 22, hence 8 spaces will be appended
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
     # Send 8 bit buffers, 1 per character.
-    clientsocket.send(bytes("Hey there!!!", "utf-8"))
-    # Close the socket
-    clientsocket.close()
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        # Send a message every 3 seconds
+        time.sleep(3)
+        msg = f"The time is: {time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}" + msg
+        clientsocket.send(bytes(msg, "utf-8"))
+    
